d07fb0bf9d048c0e8aa12df05dea6ef1c7e56121
Diego <diego-007@yandex.ru>
2026-02-16 14:42:40 +0700

    Add files via upload

---
+++ lib/RussianPost/Http/Client.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace RussianPost\Http;
+
+use RussianPost\Exception\CurlException;
+use RussianPost\Response\ApiResponse;
+
+class Client
+{
+    const METHOD_GET = 'GET';
+    const METHOD_POST = 'POST';
+    const METHOD_PUT = 'PUT';
+    const METHOD_DELETE = 'DELETE';
+
+    protected $url = 'https://otpravka-api.pochta.ru/1.0/';
+    protected $accessToken;
+    protected $login;
+    protected $password;
+
+    public function __construct($accessToken, $login, $password)
+    {
+        $this->accessToken = $accessToken;
+        $this->login = $login;
+        $this->password = $password;
+    }
+
+    public function makeRequest($path, $method = 'GET', $parameters = array(), $headers = array(), $timeout = 40) {
+        $url = $this->url . $path;
+
+        if ($method == self::METHOD_GET) {
+            $url .= '?' . http_build_query($parameters);
+        }
+
+        if (in_array($method, array(self::METHOD_POST, self::METHOD_PUT, self::METHOD_DELETE))) {
+            $headers[] = 'Content-Type: application/json;charset=UTF-8';
+            $headers[] = 'Accept: application/json;charset=UTF-8';
+        }
+
+        $headers[] = sprintf('Authorization: AccessToken %s', $this->accessToken);
+        $headers[] = sprintf('X-User-Authorization: Basic %s', base64_encode(sprintf('%s:%s', $this->login, $this->password)));
+
+        $curlHandler = curl_init();
+        //curl_setopt($curlHandler, CURLOPT_USERAGENT, 'RussianPost-API-client/1.0');
+        curl_setopt($curlHandler, CURLOPT_URL, $url);
+        curl_setopt($curlHandler, CURLOPT_RETURNTRANSFER, true);
+        //curl_setopt($curlHandler, CURLOPT_FAILONERROR, false);
+        //curl_setopt($curlHandler, CURLOPT_SSL_VERIFYPEER, false);
+        //curl_setopt($curlHandler, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curlHandler, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($curlHandler, CURLOPT_CONNECTTIMEOUT, $timeout);
+        //curl_setopt($curlHandler, CURLOPT_HTTPHEADER, array('Expect:'));
+        //curl_setopt($curlHandler, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+        //curl_setopt($curlHandler, CURLOPT_TCP_KEEPALIVE, 1);
+        //curl_setopt($curlHandler, CURLOPT_TCP_KEEPIDLE, 2);
+        //curl_setopt($curlHandler, CURLOPT_HEADER, false);
+        curl_setopt($curlHandler, CURLOPT_POST, false);
+        curl_setopt($curlHandler, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($curlHandler, CURLOPT_POSTFIELDS, array());
+        curl_setopt($curlHandler, CURLOPT_HTTPHEADER, $headers);
+
+        if ($method == self::METHOD_POST) {
+            curl_setopt($curlHandler, CURLOPT_POST, true);
+        } elseif (in_array($method, array(self::METHOD_PUT, self::METHOD_DELETE))) {
+            curl_setopt($curlHandler, CURLOPT_POST, false);
+            curl_setopt($curlHandler, CURLOPT_CUSTOMREQUEST, $method);
+        }
+
+        if ((is_array($parameters) > 0 || is_string($parameters)) && in_array($method, array(self::METHOD_POST, self::METHOD_PUT, self::METHOD_DELETE))) {
+            curl_setopt($curlHandler, CURLOPT_POSTFIELDS, $parameters);
+        }
+
+        $responseBody = curl_exec($curlHandler);
+        $statusCode = curl_getinfo($curlHandler, CURLINFO_HTTP_CODE);
+        $contentType = curl_getinfo($curlHandler, CURLINFO_CONTENT_TYPE);
+
+        $errno = curl_errno($curlHandler);
+        $error = curl_error($curlHandler);
+
+        curl_close($curlHandler);
+
+        if ($errno) {
+            throw new CurlException($error, $errno);
+        }
+
+        if ($statusCode >= 400) {
+            $error = $url.'<br />';
+            $error .= $responseBody.'<br />';
+
+            $response = @json_decode($responseBody, true);
+            if (isset($response['message'])) {
+                $error .= $response['message'].'<br />';
+            } elseif (isset($response['desc'])) {
+                $error .= $response['desc'].'<br />';
+            }
+
+            throw new CurlException($error, $statusCode);
+        }
+
+        if (stripos($contentType, 'pdf') !== false || stripos($contentType, 'zip') !== false) {
+            return $responseBody;
+        }
+
+        return new ApiResponse($statusCode, $responseBody);
+    }
+}
